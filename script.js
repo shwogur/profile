ac0e4cc0dd7394aaa435bc396d85b4f5dd500fbc
shwogur <shwogur108@gmail.com>
2025-05-15 14:33:06 +0900

    fixed

---
+++ script.js
@@ -0,0 +1,9 @@
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+  scrollTopBtn.style.display = window.scrollY > 300 ? 'block' : 'none';
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
